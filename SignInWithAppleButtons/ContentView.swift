c59bfcd4a4527eec6f81d9c0934cbd6f4430051f
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-07-19 16:06:28 +0300

    Initial Commit

---
+++ SignInWithAppleButtons/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  SignInWithAppleButtons
+//
+//  Created by Ramill Ibragimov on 19.07.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SignInWithAppleButtons/SignInWithAppleButtonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SignInWithAppleButtonsApp.swift
+//  SignInWithAppleButtons
+//
+//  Created by Ramill Ibragimov on 19.07.2020.
+//
+
+import SwiftUI
+
+@main
+struct SignInWithAppleButtonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
