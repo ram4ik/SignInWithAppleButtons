9cfcee02cbd1ced14873fca3f21438bb19731f27
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-07-19 16:12:50 +0300

    init

--- SignInWithAppleButtons/ContentView.swift
+++ SignInWithAppleButtons/ContentView.swift
@@ -9,7 +9,30 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!").padding()
+        VStack {
+            SignInWithAppleButton(.signIn,
+                onRequest: /*@START_MENU_TOKEN@*/{ request in
+                }/*@END_MENU_TOKEN@*/,
+                onCompletion: /*@START_MENU_TOKEN@*/{ result in
+                }/*@END_MENU_TOKEN@*/
+            ).frame(height: 50, alignment: .center)
+            
+            SignInWithAppleButton(.continue,
+                onRequest: /*@START_MENU_TOKEN@*/{ request in
+                }/*@END_MENU_TOKEN@*/,
+                onCompletion: /*@START_MENU_TOKEN@*/{ result in
+                }/*@END_MENU_TOKEN@*/
+            ).frame(height: 100, alignment: .center)
+            
+            SignInWithAppleButton(.signUp,
+                onRequest: /*@START_MENU_TOKEN@*/{ request in
+                }/*@END_MENU_TOKEN@*/,
+                onCompletion: /*@START_MENU_TOKEN@*/{ result in
+                }/*@END_MENU_TOKEN@*/
+            )
+            .frame(height: 50, alignment: .center)
+            .shadow(radius: 8, y: 12)
+        }.padding(.all, /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
     }
 }
 
